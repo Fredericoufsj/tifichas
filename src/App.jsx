a2c451262bd3f83f001792af81a8014269860e04
Frederico Thomaz <frederico.santos@supersim.com.br>
2024-11-07 18:56:04 -0300

    AAjuste no caminho para caso logado voltar para a home, adicionado o loading

--- src/App.jsx
+++ src/App.jsx
@@ -16,46 +16,51 @@ import LoginPage from "./pages/LoginPage"; // Novo componente de login
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Adicione um estado de loading
   const auth = getAuth();
 
   useEffect(() => {
     // Verifica o estado de autenticação do usuário
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user); // Atualiza o estado do usuário
+      setLoading(false); // Define loading como false após a verificação
     });
 
     return () => unsubscribe();
   }, [auth]);
 
-  if (!user) {
-    // Renderiza a página de login em tela cheia se o usuário não está autenticado
-    return (
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    );
+  if (loading) {
+    return <div>Carregando...</div>; // Exibe uma mensagem enquanto verifica a autenticação
   }
 
   return (
     <div className="flex">
-      <Sidebar />
-      <div className="flex-1">
+      {user ? (
+        <>
+          <Sidebar />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cargo/:cargoId" element={<CargoPage />} />
+              <Route path="/cargo/:cargoId/materia/:materiaId" element={<MateriaPage />} />
+              <Route path="/cargo/:cargoId/materia/:materiaId/topico/:topicoId" element={<TopicPage />} />
+              <Route path="/revisao" element={<ReviewPage />} />
+              <Route path="/revisao/:questionId" element={<QuestionReviewPage />} />
+              <Route path="/revisao/cargo/:cargoId" element={<ReviewMateriaPage />} />
+              <Route path="/revisao/cargo/:cargoId/materia/:materiaId" element={<ReviewTopicPage />} />
+              <Route path="/revisao/cargo/:cargoId/materia/:materiaId/topico/:topicoId" element={<ReviewQuestionPage />} />
+              <Route path="*" element={<Navigate to="/" />} /> {/* Redireciona rotas desconhecidas para a página principal */}
+            </Routes>
+          </div>
+        </>
+      ) : (
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cargo/:cargoId" element={<CargoPage />} />
-          <Route path="/cargo/:cargoId/materia/:materiaId" element={<MateriaPage />} />
-          <Route path="/cargo/:cargoId/materia/:materiaId/topico/:topicoId" element={<TopicPage />} />
-          <Route path="/revisao" element={<ReviewPage />} />
-          <Route path="/revisao/:questionId" element={<QuestionReviewPage />} />
-          <Route path="/revisao/cargo/:cargoId" element={<ReviewMateriaPage />} />
-          <Route path="/revisao/cargo/:cargoId/materia/:materiaId" element={<ReviewTopicPage />} />
-          <Route path="/revisao/cargo/:cargoId/materia/:materiaId/topico/:topicoId" element={<ReviewQuestionPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/login" />} /> {/* Redireciona rotas desconhecidas para a página de login */}
         </Routes>
-      </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
