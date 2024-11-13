44041fcfaaaead43b73b62877a41913b6c35b86b
Frederico Thomaz <frederico.santos@supersim.com.br>
2024-11-13 08:56:50 -0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/tifichas/', // Altere para o nome do seu repositório
-  plugins: [react()],
-});
